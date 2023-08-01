f9477f5dcc8cc9b01bb62d864f77c100cfb5ea7f
faithcequena2017 <141115607+faithcequena2017@users.noreply.github.com>
2023-08-01 14:51:04 +0800

    Update streamlit_app.py
    Editing header and added emoji and another menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Bluerry Outmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.title('My Parents New Healthy Dine0r')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣Omega 3 & Bluerry Outmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
 
